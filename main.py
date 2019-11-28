ff3318fd151f0dfe4d9eb1df25e55ad15a72f214
jle-santos <49331367+jle-santos@users.noreply.github.com>
2019-11-27 18:10:15 -0800

    Add buttons and joystick

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#Accept serial commands and output Joystick
-#Expecting four frames to receive from serial
-
+import pyvjoy
 import serial
 
-#Customize Parameters
-COM = "COM5"    #Com Port
-NUMFRAMES = 2   #Number of bytes to capture
+# Create joystick object
+Joystick = pyvjoy.VJoyDevice(1)
+
+# Customize Parameters
+COM = "COM5"  # Com Port
+NUMFRAMES = 4  # Number of bytes to capture
 
 print("Connecting...")
 s = serial.Serial(COM)
@@ -14,11 +15,17 @@ print("Connected to -> ", COM)
 while True:
     res = s.read(NUMFRAMES)
 
-    #Slice response into separate frames
-    X = res[:1]
-    Y = res[1:2]
+    # Slice response into separate frames
+    FrameA = res[:1]
+    FrameB = res[1:2]
+    X = res[2:3]
+    Y = res[3:4]
 
+    Face, DPad = int.from_bytes(FrameA, "big"), int.from_bytes(FrameB, "big")
     dataX, dataY = int.from_bytes(X, "big"), int.from_bytes(Y, "big")
 
-    print("Joystick Data: X: ",dataX, "| Y: ", dataY)
+    print("F: ", Face, "| D: ", DPad, "| J: X: ", dataX, "| Y: ", dataY)
 
+    # Map joystick axes
+    Joystick.set_axis(pyvjoy.HID_USAGE_X, dataX * 128)
+    Joystick.set_axis(pyvjoy.HID_USAGE_Y, dataY * 128)
