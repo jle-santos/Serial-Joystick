34f8fe6f1cb8ec5fbc742f8ee2854f67b82b3c63
jle-santos <49331367+jle-santos@users.noreply.github.com>
2019-11-29 12:51:23 -0800

    Controller support patch

--- main.py
+++ main.py
@@ -1,68 +1,93 @@
+import pyvjoy
+from time import sleep
 import serial
-import keyboard
 
-print("Serial Joystick to Key Presses V1")
-COM_NUM = input("Enter COM Port to connect: #")
 
-COM = "COM"+COM_NUM
+
+# Create joystick object
+
+Joystick = pyvjoy.VJoyDevice(1)
+
+
 
 # Customize Parameters
+
+COM = "COM4"  # Com Port
+
 NUMFRAMES = 4  # Number of bytes to capture
 
-print("Connecting...")
-s = serial.Serial(COM)
 
-if s.isOpen():
-    print("Connected to -> ", COM)
+
+print("Connecting...")
+
+s = serial.Serial(COM, baudrate=9600)
+
+print("Connected to -> ", COM)
+
+
 
 # B, X, RB, LB, Start, Select
-FaceButtons = ['c', 'v', 'e', 'q', 'z', 'x']
+
+FaceButtons = [2, 3, 6, 5, 8, 7]
+
+
 
 # Up, Down, Left, Right, A, Y
-DPad = ['w', 's', 'a', 'd', 'f', 'g']
+
+DPad = [9, 10, 11, 12, 1, 4]
+
+
 
 while True:
+
     res = s.read(NUMFRAMES)
 
+
+
     # Slice response into separate frames
+
     FrameA = res[:1]
     FrameB = res[1:2]
     X = res[2:3]
     Y = res[3:4]
 
     # Read all the data
+
     Face, DRaw = bin(int.from_bytes(FrameA, "big")), bin(int.from_bytes(FrameB, "big"))
     dataX, dataY = int.from_bytes(X, "big"), int.from_bytes(Y, "big")
 
-    # Left Stick X
-    if dataX > 200:
-        keyboard.press('d')
-    elif dataX < 100:
-        keyboard.press('a')
-    else:
-        keyboard.release('a')
-        keyboard.release('d')
+    # Slice DPad
 
-    #Right Stick Y
-    if dataY > 200:
-        keyboard.press('w')
-    elif dataY < 100:
-        keyboard.press('s')
-    else:
-        keyboard.release('w')
-        keyboard.release('s')
+    for Dir in range(len(DPad)):
 
-    for i, keyD in enumerate(DPad):
-        if Face[i+4] == '1':
-            keyboard.press(keyD)
+        if DRaw[Dir+4] == '1':
+            Joystick.set_button(DPad[Dir], 1)
         else:
-            keyboard.release(keyD)
+            Joystick.set_button(DPad[Dir], 0)
 
-    for j, keyF in enumerate(FaceButtons):
-        if DRaw[j+4] == '1':
-            keyboard.press(keyF)
+
+
+    # Slice Buttons
+
+    for Command in range(len(FaceButtons)):
+
+        if Face[Command+4] == '1':
+
+            Joystick.set_button(FaceButtons[Command], 1)
+
         else:
-            keyboard.release(keyF)
+
+            Joystick.set_button(FaceButtons[Command], 0)
+
+
+
+    # Map joystick axes
+
+    Joystick.set_axis(pyvjoy.HID_USAGE_X, dataX * 128)
+
+    Joystick.set_axis(pyvjoy.HID_USAGE_Y, dataY * 128)
 
     #Print to screen
-    output = ("F: ", Face, "| D: ", DRaw, "| J: X: ", dataX, "| Y: ", dataY)
+
+
+    #print("F: ", Face, "| D: ", DRaw, "| J: X: ", dataX, "| Y: ", dataY)
