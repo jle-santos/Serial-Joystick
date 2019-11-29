03ccc09b89e707177964598b780f8b2f0c649aae
jle-santos <49331367+jle-santos@users.noreply.github.com>
2019-11-28 20:41:13 -0800

    GUI Alpha

--- main.py
+++ main.py
@@ -1,62 +1,85 @@
+from tkinter import *
 import serial
 import keyboard
 
-# Customize Parameters
-COM = "COM5"  # Com Port
-NUMFRAMES = 4  # Number of bytes to capture
+global s
 
-print("Connecting...")
-s = serial.Serial(COM)
-print("Connected to -> ", COM)
+window = Tk()
+window.title("Serial Joystick - V1")
+window.geometry("500x500")
 
-# B, X, RB, LB, Start, Select
-FaceButtons = ['c', 'v', 'e', 'q', 'z', 'x']
+top_title = Label(window, text="Serial Joystick - 2019")
+top_title.grid(column=0, row=0)
 
-# Up, Down, Left, Right, A, Y
-DPad = ['w', 's', 'a', 'd', 'f', 'g']
+com_label = Label(window, text="Connect to COM:")
+com_label.grid(column=0, row = 1)
+com_status = Label(window, text="Not connected")
+com_status.grid(column=3, row=1)
+COM = Entry(window, width=10)
+COM.grid(column=2, row=1)
 
-while True:
-    res = s.read(NUMFRAMES)
+def enter_com():
+    s = serial.Serial(COM.get())
 
-    # Slice response into separate frames
-    FrameA = res[:1]
-    FrameB = res[1:2]
-    X = res[2:3]
-    Y = res[3:4]
-
-    # Read all the data
-    Face, DRaw = bin(int.from_bytes(FrameA, "big")), bin(int.from_bytes(FrameB, "big"))
-    dataX, dataY = int.from_bytes(X, "big"), int.from_bytes(Y, "big")
-
-    # Left Stick X
-    if dataX > 200:
-        keyboard.press('d')
-    elif dataX < 100:
-        keyboard.press('a')
+    if s.isOpen() == True:
+        com_status.configure(text="Connected!")
     else:
-        keyboard.release('a')
-        keyboard.release('d')
+        com_status.configure(window, text="Unavailable")
 
-    #Right Stick Y
-    if dataY > 200:
-        keyboard.press('w')
-    elif dataY < 100:
-        keyboard.press('s')
-    else:
-        keyboard.release('w')
-        keyboard.release('s')
+com_button = Button(window, text="Connect", command=enter_com)
+com_button.grid(column=1, row = 1)
 
-    for i, keyD in enumerate(DPad):
-        if Face[i+4] == '1':
-            keyboard.press(keyD)
+if s.isOpen():
+    # B, X, RB, LB, Start, Select
+    FaceButtons = ['c', 'v', 'e', 'q', 'z', 'x']
+
+    # Up, Down, Left, Right, A, Y
+    DPad = ['w', 's', 'a', 'd', 'f', 'g']
+
+    while True:
+        res = s.read(4)
+
+        # Slice response into separate frames
+        FrameA = res[:1]
+        FrameB = res[1:2]
+        X = res[2:3]
+        Y = res[3:4]
+
+        # Read all the data
+        Face, DRaw = bin(int.from_bytes(FrameA, "big")), bin(int.from_bytes(FrameB, "big"))
+        dataX, dataY = int.from_bytes(X, "big"), int.from_bytes(Y, "big")
+
+        # Left Stick X
+        if dataX > 200:
+            keyboard.press('d')
+        elif dataX < 100:
+            keyboard.press('a')
         else:
-            keyboard.release(keyD)
+            keyboard.release('a')
+            keyboard.release('d')
 
-    for j, keyF in enumerate(FaceButtons):
-        if DRaw[j+4] == '1':
-            keyboard.press(keyF)
+        #Right Stick Y
+        if dataY > 200:
+            keyboard.press('w')
+        elif dataY < 100:
+            keyboard.press('s')
         else:
-            keyboard.release(keyF)
+            keyboard.release('w')
+            keyboard.release('s')
 
-    #Print to screen
-    output = ("F: ", Face, "| D: ", DRaw, "| J: X: ", dataX, "| Y: ", dataY)
+        for i, keyD in enumerate(DPad):
+            if Face[i+4] == '1':
+                keyboard.press(keyD)
+            else:
+                keyboard.release(keyD)
+
+        for j, keyF in enumerate(FaceButtons):
+            if DRaw[j+4] == '1':
+                keyboard.press(keyF)
+            else:
+                keyboard.release(keyF)
+
+        #Print to screen
+        output = ("F: ", Face, "| D: ", DRaw, "| J: X: ", dataX, "| Y: ", dataY)
+
+window.mainloop()
