94a87f0c8d06fe1a9801df3558c66467f76c2f18
jle-santos <49331367+jle-santos@users.noreply.github.com>
2019-11-30 09:52:09 -0800

    Dual controller support added

--- main.py
+++ main.py
@@ -2,48 +2,45 @@ import pyvjoy
 from time import sleep
 import serial
 
-
-
-# Create joystick object
-
-Joystick = pyvjoy.VJoyDevice(1)
-
-
-
-# Customize Parameters
-
-COM = "COM4"  # Com Port
-
+# Constants
 NUMFRAMES = 4  # Number of bytes to capture
 
+# Create joystick object
+Joystick = pyvjoy.VJoyDevice(1)
+Joystick2 = pyvjoy.VJoyDevice(2)
 
+# Customize Parameters
+# Ask for Input
+print("Serial Joystick V1 - ELEX 7820")
+numCtrl = input("How many controllers to connect? (MAX:2): ")
+COM_LIST = []
+
+for i in range(int(numCtrl)):
+    print("Controller ", i)
+    COM_PORT = input("Enter Com Port:")
+    COM_LIST.append(COM_PORT)
 
 print("Connecting...")
 
-s = serial.Serial(COM, baudrate=9600)
+s = serial.Serial(COM_LIST[0], baudrate=9600)
 
-print("Connected to -> ", COM)
+if numCtrl == 2:
+    s2 = serial.Serial(COM_LIST[1], baudrate=9600)
+
+for com in COM_LIST:
+    print("Connected to: ", com)
+
 
 
 
 # B, X, RB, LB, Start, Select
-
 FaceButtons = [2, 3, 6, 5, 8, 7]
 
-
-
 # Up, Down, Left, Right, A, Y
-
 DPad = [9, 10, 11, 12, 1, 4]
 
 
-
-while True:
-
-    res = s.read(NUMFRAMES)
-
-
-
+def SendCommands(res, Joy):
     # Slice response into separate frames
 
     FrameA = res[:1]
@@ -60,34 +57,38 @@ while True:
 
     for Dir in range(len(DPad)):
 
-        if DRaw[Dir+4] == '1':
-            Joystick.set_button(DPad[Dir], 1)
+        if DRaw[Dir + 4] == '1':
+            Joy.set_button(DPad[Dir], 1)
         else:
-            Joystick.set_button(DPad[Dir], 0)
-
-
+            Joy.set_button(DPad[Dir], 0)
 
     # Slice Buttons
 
     for Command in range(len(FaceButtons)):
 
-        if Face[Command+4] == '1':
+        if Face[Command + 4] == '1':
 
-            Joystick.set_button(FaceButtons[Command], 1)
+            Joy.set_button(FaceButtons[Command], 1)
 
         else:
 
-            Joystick.set_button(FaceButtons[Command], 0)
-
-
+            Joy.set_button(FaceButtons[Command], 0)
 
     # Map joystick axes
 
-    Joystick.set_axis(pyvjoy.HID_USAGE_X, dataX * 128)
+    Joy.set_axis(pyvjoy.HID_USAGE_X, dataX * 128)
 
-    Joystick.set_axis(pyvjoy.HID_USAGE_Y, dataY * 128)
+    Joy.set_axis(pyvjoy.HID_USAGE_Y, dataY * 128)
 
-    #Print to screen
+    # Print to screen
+    output = ("F: ", Face, "| D: ", DRaw, "| J: X: ", dataX, "| Y: ", dataY)
 
+    return output
 
-    #print("F: ", Face, "| D: ", DRaw, "| J: X: ", dataX, "| Y: ", dataY)
+while True:
+    res = s.read(NUMFRAMES)
+    res2 = s2.read(NUMFRAMES)
+
+    output1 = SendCommands(res, Joystick)
+    output2 = SendCommands(res2, Joystick2)
+    #print(output1, output2)
